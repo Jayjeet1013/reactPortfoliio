633040645b3dee4127a15a60836fe709a6015b7b
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-01 20:54:52 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 function  App()  {
   return (
     <div className="App">
-       <h1>chalo kuch to kre</h1>
+     
        <Home/>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import Home from './Home'
+
 import Contact from './Contact'
 import About from './About'
 import Onofflight from './Onofflight'
+
 function Header() {
   return (
     <>
-       
-      <Home/>
+       <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact{Contact}</li>
+        <li>Onofflight</li>
+       </ul>
+    
       <Contact/>
       <About/>
       <Onofflight/>
