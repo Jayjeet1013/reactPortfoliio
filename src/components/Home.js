3562b0f912efc8b43e78afdb2a4261c5e61dc99d
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-02 21:40:25 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Image from './1.jpeg'
 import Header from './Header'
+import './Home.css'
 
 export default function Home() {
   return (
 <>
    
     <Header />
+    <img className='img' src={Image} />
 </>
   )
 }
