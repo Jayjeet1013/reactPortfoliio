8aab4e50c4cf57a828c9f215986985c1d4cae81f
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-02 21:09:14 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Header.css'
 import Contact from './Contact'
 import About from './About'
 import Onofflight from './Onofflight'
@@ -7,16 +7,15 @@ import Onofflight from './Onofflight'
 function Header() {
   return (
     <>
-       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact{Contact}</li>
-        <li>Onofflight</li>
+       <ul>    <li><a href='<Onofflight/>'>Onofflight</a></li>
+       
+        <li><a href='<About/>'>About</a></li>
+        <li> <a href='<Contact/>'>Contact</a></li>
+     <li><a href='<Home/>'>Home</a></li>
        </ul>
     
-      <Contact/>
-      <About/>
-      <Onofflight/>
+    
+  
     </>
   )
 }
