3454673f222eb3041225a35c9d1517f2d87a5d15
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-05 05:07:58 +0530

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ export default function Projects() {
   return (
   <>
      
-    <h3>These are my some lovely Projects ever.</h3>
+    <h3 className='h3pro'>These are my some lovely Projects ever.</h3>
     <a  href='https://spotifyjay.netlify.app/'><button className='btnprojects'>Link</button></a>
 
   </>
