49c0ff46c27f23d4dcbefa949b14c87aae8f89f1
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-04 22:32:44 +0530

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Projects.css'
 
 export default function Projects() {
   return (
   <>
+     
     <h3>These are my some lovely Projects ever.</h3>
-    <a href=''></a>
+    <a  href='https://spotifyjay.netlify.app/'><button className='btnprojects'>Link</button></a>
 
   </>
   )
