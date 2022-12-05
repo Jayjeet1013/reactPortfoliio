0961e4723f0bae15f23cfba19abaf7708028022e
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-06 01:51:37 +0530

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Contact.css'
 
 export default function Contact() {
   return (
     <>
       <h3 className='h3Con'>Contact Details</h3>
-      <a href='https://www.linkedin.com/in/jayjeet-kumar-71a7751ba/'>  linkedin</a>
+      <a  href='https://www.linkedin.com/in/jayjeet-kumar-71a7751ba/'> linkedin</a>
+   
     </>
   )
 }
