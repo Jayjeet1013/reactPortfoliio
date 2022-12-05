2883377a27f98095eec604a5521cc4c0f869c2f2
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-05 05:33:53 +0530

    update in PRoject sections

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Projects.css'
+import Pro1 from './pro1.jpeg'
 
 export default function Projects() {
   return (
   <>
      
     <h3 className='h3pro'>These are my some lovely Projects ever.</h3>
-    <a  href='https://spotifyjay.netlify.app/'><button className='btnprojects'>Link</button></a>
+    <div className='proj1'>
+     
+    <img src={Pro1} alt='prop'/>   <a  href='https://spotifyjay.netlify.app/' ><button className='btnprojects'>Link</button></a> <h3>Spotify Clone</h3>
+ 
+    <p className='ppro'>This is a Spotify clone. Made by using HTML,CSS and Javascript.Here, you can listen one DJ Naggin Song</p>
+    
+     </div>
+   
 
   </>
   )
