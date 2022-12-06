331600fb8a6ad8aaf86b8f5d5eabc38e60975168
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-07 01:16:46 +0530

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     <>
       <h3 className='h3Con'>Contact Details</h3>
       <a  className='icon' href='https://www.linkedin.com/in/jayjeet-kumar-71a7751ba/'> linkedin</a>
-      <a  className='icon' href='https://twitter.com/Jayjeet_100'> Twitter </a>
+      <a  className='icontw' href='https://twitter.com/Jayjeet_100'> Twitter </a>
     </>
   )
 }
