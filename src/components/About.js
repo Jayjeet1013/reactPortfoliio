3b652823c4eeed61189dd7b7e82e14203f411738
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-09 23:12:36 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
    <>
     <h3>Currently, I am pursuing B.Tech in Computer science</h3>
-    
+    <p>I try to learn and explore Web3.</p>
    </>
   )
 }
