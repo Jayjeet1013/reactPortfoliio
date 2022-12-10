1344aab25d8465d5a4f2488dbe882c3f94c3f25f
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-10 07:07:57 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function About() {
   return (
    <>
+   <h2>About ME...</h2>
     <h3>Currently, I am pursuing B.Tech in Computer science</h3>
     <p>I try to learn and explore Web3.</p>
    </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
         <li><a href='<Onofflight/>'>Onofflight</a></li>
        
         <li><a href='<About/>'>About</a></li>
-        <li><a href='<Projects/>' ></a>Projects</li>
+        <li><a href='<Projects/>' >Projects</a></li>
         <li> <a href='<Contact/>'>Contact</a></li>
      <li><a href='<Home/>'>Home</a></li>
        </ul>
