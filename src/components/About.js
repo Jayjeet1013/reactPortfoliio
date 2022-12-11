a1757af4c67fd73f46a70ca7ca006eaf8b326284
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-11 14:46:17 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,11 @@ export default function About() {
   return (
    <>
    <h2>About ME...</h2>
-    <h3>Currently, I am pursuing B.Tech in Computer science</h3>
+    <h3>Currently, I am pursuing B.Tech in Computer science
+    <p>From Amity University Patna </p>
+    <h4>Live in Bihar</h4>
+    <h5>Only want to do Remote Work</h5>
+    </h3>
     <p>I try to learn and explore Web3.</p>
    </>
   )
