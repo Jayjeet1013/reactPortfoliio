6985ab37cc18995e991fc05dba0000d404839028
jayjeet kumar <jayjeet1013@gmail.com>
2022-12-15 20:43:53 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 
 import React from "react";
 import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Projects from "./components/Projects";
+
 
 
 function  App()  {
